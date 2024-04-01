e549c776c21242c49de7b64a44f6d384dbe88c24
vikaskushwah738 <vikaskushwah738@gmail.com>
2024-04-01 08:50:14 +0530

    adding new file

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,18 +1,21 @@
-
+import { Meteors } from "@/components/ui/meteors";
 const contact = () => {
   return (
-    <div className="bg-black h-[40rem]">
-      <p className="text-5xl font-semibold pt-48 text-white text-center">Contact Us</p>
+    <div className="bg-gray-900 h-[40rem]">
+      <p className="text-5xl font-semibold pt-40 text-white text-center">Contact Us</p>
       <p className="mx-auto text-center pt-7 text-white w-1/2 pb-6">Whether you have questions, feedback, or simply want to say hello, we're here to assist you.
         At The Hungry Hideaway, we value your input and strive to provide exceptional service. Your satisfaction
         is our top priority, and we're committed to ensuring that your dining experience with us exceeds your
         expectations.</p>
-      <div className="flex justify-center w-1/2">
-        <form className="text-white mx-auto">
-          <p className="pb-5"><input type="text" className="w-full " /></p>
-          <p><textarea name="" id="" className="w-full"></textarea></p>
+      
+        <form className="text-white mx-auto w-1/2 flex justify-center flex-col">
+          <div className="pb-6"><input type="text" className="w-full bg-black p-3 rounded-md" placeholder="your e-mail address"/></div>
+          <div className="pb-6"><textarea name="" id="" className="w-full bg-black p-3 rounded-md " placeholder="your message"></textarea></div>
+          <div className="pb-6 flex justify-center items-center"><input type="submit" value={'submit'} className="w-32 bg-black border-2 border-white hover:bg-gray-900 p-2 rounded-md" /></div>
+          
         </form>
-      </div>
+       
+        <Meteors number={50} />
     </div>
 
   )
